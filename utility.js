9f872c1dc55f87e4f6e7afef01be24ec48d185b9
SunnyPandita123 <pandita.sunny123@gmail.com>
2023-05-08 15:34:08 +0530

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,3 +1,9 @@
 export const checkEmail = (email) => {
  return email.includes("@"); 
-}
+};
+
+const phoneNumberRegex = /^(\+0?1)?\(?\d{3}\)?\d{3}\d{4}$/;
+
+export const checkPhone = (phone) => {
+ return phoneNumberRegex.test(phone);
+};
